8dd931719e8a2c97d21dd17c943a7ba2cd1b39e5
Palash Das <palash.das@sap.com>
2021-03-25 11:03:18 +0530

    Dynamic changes

--- src/com/graph_implementation/dynamicgraph.java
+++ src/com/graph_implementation/dynamicgraph.java
@@ -3,11 +3,11 @@ package com.graph_implementation;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Edge
-{
+class Edge {
 	int startVertex;
 	int endVertex;
 	double weight;
+
 	public Edge(int startVertex, int endVertex, double weight) {
 		super();
 		this.startVertex = startVertex;
@@ -17,74 +17,93 @@ class Edge
 }
 
 public class dynamicgraph {
-	
-public static void main(String[] args) {
-	
 
-	Scanner sc = new Scanner(System.in);
+	static int[][] adjacency_matrix = null;
+	static ArrayList<Edge> edgelist = new ArrayList<Edge>();
 
-	int vertex = 5;
-	
-	int[][] matrix = new int[vertex+1][vertex+1];
-	ArrayList<Edge> edgelist = new ArrayList<Edge>();
-	
-	edgelist.add(new Edge(1,2,5.0));
-	edgelist.add(new Edge(2,2,5.0));
-	edgelist.add(new Edge(3,4,5.0));
-	edgelist.add(new Edge(3,2,5.0));
-	edgelist.add(new Edge(3,3,5.0));
-	edgelist.add(new Edge(4,5,5.0));
-	
-	for(int i=0;i<edgelist.size();i++)
-	{
-		Edge currentEdge = edgelist.get(i);
-		int startVertex = currentEdge.startVertex;
-		int endVertex =currentEdge.endVertex;
-		matrix[startVertex][endVertex]=1;
-	}
-	
-	for(int i = 1;i<=vertex;i++)
-	{
-		for(int j=1;j<=vertex;j++)
-		{
-			System.out.print(matrix[i][j]+"");
-		}
-		System.out.println();
-	}
-	
-	System.out.println("Enter the 2 nodes for which you want to delete the edge");
-	int delnode1 = sc.nextInt();
-	int delnode2 = sc.nextInt();
-	for(int i=0;i<edgelist.size();i++)
-	{
-		Edge currentEdge = edgelist.get(i);
-		int startVertex = currentEdge.startVertex;
-		int endVertex =currentEdge.endVertex;
-		
-		if(currentEdge.startVertex == delnode1 && currentEdge.endVertex == delnode2)
-		{
-			edgelist.remove(i);
-			matrix[startVertex][endVertex] = 0;
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("Enter 1. Add nodes 2. Add edges 3. Delete Edge  4. Display the graph 5.Exit");
+			int ch = sc.nextInt();
+			switch (ch) {
+			case 1:
+				System.out.println("Enter number of nodes");
+				int nodes = sc.nextInt();
+				add_nodes(nodes);
+				break;
+			case 2:
+				System.out.println("Enter the start node , end node and weight of the edge ");
+				int start_node = sc.nextInt();
+				int end_node = sc.nextInt();
+				double weight = sc.nextDouble();
+				add_edge(start_node, end_node, weight);
+				break;
+			case 3:
+				System.out.println("Enter the 2 nodes for which you want to delete the edge");
+				int delnode1 = sc.nextInt();
+				int delnode2 = sc.nextInt();
+				delete_node(delnode1, delnode2);
+				break;
+			case 4:
+				print_node();
+				break;
+			case 5:
+				System.exit(1);
+
+			}
+
 		}
 	}
-	System.out.println("Enter the 2 nodes for which you want to insert the edge");
-	int addnode1 = sc.nextInt();
-	int addnode2 = sc.nextInt();
-	
-	edgelist.add(new Edge(addnode1,addnode2,5.0));
-	matrix[addnode1][addnode2]=1;
-	
-	
-	System.out.println("TaDa----------------------------");
-	for(int i = 1;i<=vertex;i++)
-	{
-		for(int j=1;j<=vertex;j++)
-		{
-			System.out.print(matrix[i][j]+"");
-		}
-		System.out.println();
-	}
-	
-	}	
 
+	private static void print_node() {
+
+		for (int i = 1; i < adjacency_matrix.length; i++) {
+			for (int j = 1; j < adjacency_matrix.length; j++) {
+				if(adjacency_matrix[i][j]==1)
+				System.out.print("\t" + i + "->" + j + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+	private static void delete_node(int delnode1, int delnode2) {
+		for (int i = 0; i < edgelist.size(); i++) {
+			Edge currentEdge = edgelist.get(i);
+			int startVertex = currentEdge.startVertex;
+			int endVertex = currentEdge.endVertex;
+
+			if (currentEdge.startVertex == delnode1 && currentEdge.endVertex == delnode2) {
+				edgelist.remove(i);
+				adjacency_matrix[startVertex][endVertex] = 0;
+			}
+		}
+
+	}
+
+	private static void add_edge(int start_node, int end_node, double weight) {
+		edgelist.add(new Edge(start_node, end_node, weight));
+		adjacency_matrix[start_node][end_node] = 1;
+
+	}
+
+	private static void add_nodes(int nodes) {
+
+		if (adjacency_matrix == null) {
+			adjacency_matrix = new int[nodes + 1][nodes + 1];
+		} else {
+			int[][] temp = new int[nodes + 1][nodes + 1];
+
+			for (int i = 1; i < adjacency_matrix.length; i++) {
+				for (int j = 1; j < adjacency_matrix.length; j++) {
+					temp[i][j] = adjacency_matrix[i][j];
+
+				}
+
+			}
+			adjacency_matrix = temp;
+		}
+	}
 }
