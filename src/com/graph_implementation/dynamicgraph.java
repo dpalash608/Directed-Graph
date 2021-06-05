e44652e9d2a06f2526a551e47a529b1566b397b3
Palash Das <palash.das@sap.com>
2021-06-05 19:32:26 +0530

    n

--- src/com/graph_implementation/dynamicgraph.java
+++ src/com/graph_implementation/dynamicgraph.java
@@ -1,6 +1,8 @@
 package com.graph_implementation;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Scanner;
 
 class Edge {
@@ -53,18 +55,96 @@ public class dynamicgraph {
 			case 5:
 				sc.close();
 				System.exit(1);
+			case 6:				
+				dijkstra_GetMinDistances(1);
+			break;
 
 			}
 
 		}
 	}
+	
+	static int vertices =5 ;
+	
+	 //get the vertex with minimum distance which is not included in SPT
+    static int getMinimumVertex(boolean [] mst, int [] key){
+        int minKey = Integer.MAX_VALUE;
+        int vertex = 1;
+        for (int i = 1; i <vertices ; i++) {
+            if(mst[i]==false && minKey>key[i]){
+                minKey = key[i];
+                vertex = i;
+            }
+        }
+        return vertex;
+    }
+
+    public static void dijkstra_GetMinDistances(int sourceVertex){
+        boolean[] spt = new boolean[vertices];
+        int [] distance = new int[vertices];
+        int INFINITY = Integer.MAX_VALUE;
+
+        //Initialize all the distance to infinity
+        for (int i = 1; i <vertices ; i++) {
+            distance[i] = INFINITY;
+        }
+
+        //start from the vertex 0
+        distance[sourceVertex] = 0;
+
+        //create SPT
+        for (int i = 1; i <vertices ; i++) {
+
+            //get the vertex with the minimum distance
+            int vertex_U = getMinimumVertex(spt, distance);
+
+            //include this vertex in SPT
+            spt[vertex_U] = true;
+
+            //iterate through all the adjacent vertices of above vertex and update the keys
+            for (int vertex_V = 0; vertex_V <vertices ; vertex_V++) {
+                //check of the edge between vertex_U and vertex_V
+                if(adjacency_matrix[vertex_U][vertex_V]>0){
+                    //check if this vertex 'vertex_V' already in spt and
+                    // if distance[vertex_V]!=Infinity
+
+                    if(spt[vertex_V]==false && adjacency_matrix[vertex_U][vertex_V]!=INFINITY){
+                        //check if distance needs an update or not
+                        //means check total weight from source to vertex_V is less than
+                        //the current distance value, if yes then update the distance
+
+                        int newKey =  adjacency_matrix[vertex_U][vertex_V] + distance[vertex_U];
+                        if(newKey<distance[vertex_V])
+                            distance[vertex_V] = newKey;
+                    }
+                }
+            }
+        }
+        //print shortest path tree
+        printDijkstra(sourceVertex, distance);
+    }
+
+    public static void printDijkstra(int sourceVertex, int [] key){
+        System.out.println("Dijkstra Algorithm: (Adjacency Matrix)");
+        for (int i = 1; i <vertices ; i++) {
+            System.out.println("Source Vertex: " + sourceVertex + " to vertex " +   + i +
+                    " distance: " + key[i]);
+        }
+    }
 
 	private static void print_node() {
 
 		for (int i = 1; i < adjacency_matrix.length; i++) {
 			for (int j = 1; j < adjacency_matrix.length; j++) {
-				if(adjacency_matrix[i][j]==1)
-				System.out.print("\t" + i + "->" + j + "\t");
+				if(adjacency_matrix[i][j]>0)
+				System.out.print("\t" + i + "->" + j + "\t" +adjacency_matrix[i][j] +"\t");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 1; i < adjacency_matrix.length; i++) {
+			for (int j = 1; j < adjacency_matrix.length; j++) {
+				System.out.print(adjacency_matrix[i][j]+"\t");
 			}
 			System.out.println();
 		}
@@ -86,7 +166,7 @@ public class dynamicgraph {
 
 	private static void add_edge(int start_node, int end_node, double weight) {
 		edgelist.add(new Edge(start_node, end_node, weight));
-		adjacency_matrix[start_node][end_node] = 1;
+		adjacency_matrix[start_node][end_node] = (int) weight ;
 
 	}
 
